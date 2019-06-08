bc5180a6137a57f45d6992f7ef9f48f7ee7c1419
slorenzorodriguez <43774760+slorenzorodriguez@users.noreply.github.com>
2019-06-08 12:24:07 +0200

    Update Maxasita.kt

--- src/Masaxista.kt
+++ src/Masaxista.kt
@@ -5,12 +5,15 @@ class Masaxista(nome: String, apelidos: String, id: Int, edad: Int, var anosExpe
     Plantilla(nome, apelidos, id, edad) {
 
     override fun viaxar() {
+        //método heredade de Plantilla
         JOptionPane.showMessageDialog(null, "Viaxa o masaxista: " + super.nome!!)
     }
 
     fun darMasaxes() {}
+        //método de la clase
 
     override fun toString(): String {
+        ////método toString que imprime las variables heredadas de la superclase Plantilla y las variables propias
         return "Datos masaxista: \n " + super.toString() + "\nAnosExperiencia = " + anosExperiencia + "\nTitulacion = " + titulacion
     }
 
