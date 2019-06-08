f83af8fc47aacc404ff20064340588b01bea7868
slorenzorodriguez <43774760+slorenzorodriguez@users.noreply.github.com>
2019-06-08 12:25:24 +0200

    Update xogador.kt

--- src/Xogador.kt
+++ src/Xogador.kt
@@ -4,13 +4,16 @@ import javax.swing.JOptionPane
 class Xogador(nome: String, apelidos: String, id: Int, edad: Int, var dorsal: Int, var demarcacion: String?) :
     Plantilla(nome, apelidos, id, edad) {
 
-    private fun xogarPartido() {}
-    private fun entrenar() {}
-    override fun viaxar() {
+    private fun xogarPartido() {} //método propio de la clase
+        
+    private fun entrenar() {} //método propio de la clase
+        
+    override fun viaxar() { //método heredado de Seleccion
         JOptionPane.showMessageDialog(null, "Viaxan os xogadores: " + super.nome!!)
     }
 
     override fun toString(): String {
+        //método toString que imprime las variables propias y heredadas de la superclase Plantilla
         return "Ficha do xogador: \n" + super.toString() + "\nDorsal = " + dorsal + "\nDemarcacion = " + demarcacion
     }
 
