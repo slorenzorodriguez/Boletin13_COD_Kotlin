8b69ccb66a2a37d6f2a851d1da175063ab56fa59
slorenzorodriguez <43774760+slorenzorodriguez@users.noreply.github.com>
2019-06-08 12:21:58 +0200

    Update App.kt

--- src/App.kt
+++ src/App.kt
@@ -1,18 +1,20 @@
 import javax.swing.JOptionPane
 
 fun main(args: Array<String>) {
+    
+    //añadimos Xogador,Adestrador, Masaxista y Plantilla
     val obx = Plantilla("Manuel", "Garcia Roo", 30, 32)
     val obxX = Xogador("Martín", "Perez Lorenzo", 6, 24, 10, "Delantero")
     val obxB = Adestrador("Federico", "Martinez Urgal", 45, 57, "3456")
     val obxM = Masaxista("Matías", "Fernandez Vázquez", 65, 46, 22, "Técnico")
 
-    obx.concentrarse()
-    obx.viaxar()
-    JOptionPane.showMessageDialog(null, obx.toString())
-    obxX.viaxar()
-    JOptionPane.showMessageDialog(null, obxX.toString())
-    obxB.viaxar()
-    JOptionPane.showMessageDialog(null, obxB.toString())
-    obxM.viaxar()
-    JOptionPane.showMessageDialog(null, obxM.toString())
+    plant.concentrarse()
+    plant.viaxar()
+    JOptionPane.showMessageDialog(null, plant.toString())
+    xoga.viaxar()
+    JOptionPane.showMessageDialog(null, xoga.toString())
+    adestra.viaxar()
+    JOptionPane.showMessageDialog(null, adestra.toString())
+    masax.viaxar()
+    JOptionPane.showMessageDialog(null, masax.toString())
 }
