0cc42f6d80dec13e0b961d7d54483c0a1c0e77df
slorenzorodriguez <43774760+slorenzorodriguez@users.noreply.github.com>
2019-06-08 12:19:25 +0200

    Update Adestrador.kt

--- src/Adestrador.kt
+++ src/Adestrador.kt
@@ -3,13 +3,17 @@ import javax.swing.JOptionPane
 class Adestrador(nome: String, apelidos: String, id: Int, edad: Int, var idFederacion: String?) :
     Plantilla(nome, apelidos, id, edad) {
 
-    private fun dirixirPartido() {}
-    private fun dirixirAdestramento() {}
+    private fun dirixirPartido() {} //método de la clase
+        
+    private fun dirixirAdestramento() {} //método de la clase
+        
     override fun viaxar() {
+        //método heredado de Plantilla
         JOptionPane.showMessageDialog(null, "Viaxa o entrenador: " + super.nome!!)
     }
 
     override fun toString(): String {
+         //método toString que imprime las variables heredadas de la superclase Plantilla y las variables propias
         return "Ficha do adestrador: \n" + super.toString() + "\nIdFederacion = " + idFederacion
     }
 
